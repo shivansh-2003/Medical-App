b1db3745421f639279f7555aa9417a314b05d303
shivansh <shivansh.m2003@gmail.com>
2024-04-12 11:35:05 +0530

    deleting image path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Welcome To Medical App")
-image_path='/Users/shivanshmahajan/Desktop/DataScinece/project/Medical App/combined/Images/why-medical-tourism-is-on-the-rise.webp'
-st.image(image_path, use_column_width=True)
+#image_path='/Users/shivanshmahajan/Desktop/DataScinece/project/Medical App/combined/Images/why-medical-tourism-is-on-the-rise.webp'
+#st.image(image_path, use_column_width=True)
